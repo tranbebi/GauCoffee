1ac954337a01a620d4fabe7a0278bd7e4d148fb2
tranbebi <bit92785@gmail.com>
2024-08-30 23:46:58 +0700

    tai len tren

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './App.css'
+function App() {
+  return (
+  <> 
+  <div className='contents-wrapper'>
+    <header>header</header>
+    <main>
+      <div className='slider'>
+      slider
+      </div>
+      <div className='story'>
+      story
+      </div>
+      <div className='featured_mugr'>
+      featured_mugr
+      </div>
+      <div className='more_products'>
+      more_products
+      </div>
+      <div className='coffee-magarine'>
+      coffee-magarine
+      </div>
+      <div className='lifestyle_stories'>
+      lifestyle_stories
+      </div>
+      <div className='subscribe-us'>
+      subscribe-us
+      </div>
+    </main>
+    <footer></footer>
+  </div>
+  </>
+  )
+}
+
+export default App
